2e6bb1b6db17e11550e882ea1acac00994262d2d
Jalal Qusailah <jalalqusailah@gmail.com>
2025-07-25 00:02:23 +0300

    link app with firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'my_bookly_app.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyBooklyApp());
 }
