d51310b06483d709e06a8b7405111b73d8e31745
Jalal Qusailah <jalalqusailah@gmail.com>
2025-07-29 02:28:50 +0300

    create use case abstract with param

--- lib/features/home/domain/usecases/featch_newset_books_use_case.dart
+++ lib/features/home/domain/usecases/featch_newset_books_use_case.dart
@@ -4,12 +4,12 @@ import 'package:my_bookly_app/core/utils/use_cases/use_case.dart';
 import 'package:my_bookly_app/features/home/domain/entities/book_entity.dart';
 import 'package:my_bookly_app/features/home/domain/repositories/home_repository.dart';
 
-class FeatchFeaturedBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
+class FeatchNewsetBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
   final HomeRepository homeRepository;
 
-  FeatchFeaturedBooksUseCase(this.homeRepository);
+  FeatchNewsetBooksUseCase(this.homeRepository);
   @override
   Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
-    return await homeRepository.fetchFearuredBooks();
+    return await homeRepository.fechNewsetBooks();
   }
 }
