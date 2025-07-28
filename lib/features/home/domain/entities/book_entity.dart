19bc006ea1e15c9fd73d63c56beaf9f40b562b1b
Jalal Qusailah <jalalqusailah@gmail.com>
2025-07-29 01:20:14 +0300

    create BookEntity

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,9 @@
+class BookEntity {
+  final String bookImg;
+  final String auterName;
+  final String name;
+  final num price;
+  final num raite;
+
+  BookEntity(this.bookImg, this.auterName, this.name, this.price, this.raite);
+}
